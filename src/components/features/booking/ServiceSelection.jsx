844fbb03095e74308c76fff6dcdb1055916efae7
webdunyosi <alimardontoshpulatov1978@gmail.com>
2026-02-11 22:56:27 +0500

    new stuctura

--- src/components/features/booking/ServiceSelection.jsx
+++ src/components/features/booking/ServiceSelection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { formatPrice } from '../utils/format';
+import { formatPrice } from '../../../utils/format';
 
 const ServiceSelection = ({ services, selectedService, onSelectService }) => {
   return (
